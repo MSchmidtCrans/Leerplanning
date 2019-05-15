c0572f76eda335745a53d8eca8e90b725d139907
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-15 11:27:39 +0200

    Urgency is now added dynamically

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ $(document).ready(function(){
 $("#actSubmitBtn").click(function() {
     let actValue = $("#inputAct").val();
     let actUrgency = $("input[name='urgentie']:checked").val();
-    $("#rowOne").append("<p><span>" + actValue + "</span></p>");
+
+    //Check if text field isn't empty before adding activity
+    if (actValue != "") {
+        if (actUrgency == "laag") {
+    $("#rowOne").append("<p><span class='urgencyLow'>" + actValue + "</span></p>");
+        } else if(actUrgency == "middel") {
+            $("#rowOne").append("<p><span class='urgencyNormal'>" + actValue + "</span></p>");
+        } else {
+            $("#rowOne").append("<p><span class='urgencyHigh'>" + actValue + "</span></p>");
+        }
 
     //Close Modal and reset modal fields 
     $("#inputAct").val("");
     $("input[name='urgentie']:checked").removeProp("checked");
     $("#modalContainer").css("display", "none");
+    } else {
+        alert("Graag text invullen bij het activiteit veld!!");
+    }
     
     
 
